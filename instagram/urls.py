06dd8f8d1e4d1df2327df8b9cb38c42041830883
carolyne <pcmaunda@gmail.com>
2022-06-13 21:40:19 +0300

    created sign -in and logout html

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
-
-from django.contrib import admin
 from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',views.home, name='index'),
+    path('registration/',views.register, name='registration'),
+    path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
 ]
 
--- myinstagram/settings.py
+++ myinstagram/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'instagram',
-    'crispy_forms'
+    'crispy_forms',
+    'bootstrap4'
     
 ]
 
